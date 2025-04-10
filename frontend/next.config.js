2e16556e4d0a2da764f1c244dc96c70d66753601
A2Pro <aayushpalai@gmail.com>
2025-04-09 21:35:29 -0700

    x

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://10.244.30.95:8080/api/:path*',
+        destination: 'http://winvite-backend/api/:path*',
       },
     ];
   },
